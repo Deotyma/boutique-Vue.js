89e6e285daa0c49721d0b791467a1bc041cc6cf6
Ewa Kadziolka <kalistaewa@gmail.com>
2020-06-23 00:17:32 +0200

    products layout

--- src/components/Shop/ShopProductItem.vue
+++ src/components/Shop/ShopProductItem.vue
@@ -1,15 +1,43 @@
 <template>
-  <div>
+  <div class="w-25 p-3 product-container">
+      <div class="d-flex flex-row justify-content-center">
+          <img style="height:150px;" :src="product.img">
+      </div>
+      <div>
+          <h3>{{product.title}}</h3>
+      </div>
+      <hr>
+      <div>
+          <p>{{product.description}}</p>
+      </div>
+      <div>
+          <span>{{product.price}}</span>
+          <button class="btn btn-primary btn-sm float-right">Commander</button>
+      </div>
+      
   </div>
 </template>
 
 <script>
 export default {
-  components: {
+  data(){
+    return {
+        product: {
+            img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.a3cTF2kZNwANt9pgP_19WgHaF3%26pid%3DApi&f=1',
+            title: 'MacBook',
+            description: 'Incroyablement fin et léger, le MacBook Air monte encore en gamme.',
+            price: '1500€'
+        }    
+    }
   }
 }
 </script>
 
 <style>
-
+.product-container{
+    height: 400px;
+}
+p{
+    font-size: 12px
+}
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="">
+      <img src="../assets/logo.png" alt="" width="30" height="30">
+      Dyma
+    </a>
+    <button class="navbar-toggler">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav">
+        <li class="nav-item"><a class ="nav-link" href="">Boutique</a></li>
+        <li class="nav-item"><a class ="nav-link" href="">Admin</a></li>
+
+      </ul>
+    </div>
+
+  </nav>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
