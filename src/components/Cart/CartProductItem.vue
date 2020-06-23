7f1ee9564bab405cb0b7cd29894600540564c105
Ewa Kadziolka <kalistaewa@gmail.com>
2020-06-23 13:25:54 +0200

    cart

--- src/components/Cart/CartProductItem.vue
+++ src/components/Cart/CartProductItem.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
+  <div class="d-flex fles-row justify-content-between p-2 px-5 ">
+    <span>{{item.title}}</span>
+    <span><strong>{{item.price}}€</strong></span>
   </div>
 </template>
 
 <script>
 export default {
-  components: {
-  }
+  props: ['item']
 }
 </script>
 
 <style>
-
 </style>
--- src/components/Cart/CartProductsList.vue
+++ src/components/Cart/CartProductsList.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
+  <div class="d-flex flex-column flex-wrap p-1">
+    <cart-product-item v-for="item in cart" :key="item.id" :item="item"></cart-product-item>
   </div>
 </template>
 
 <script>
+import CartProductItem from './CartProductItem.vue';
 export default {
   components: {
-  }
+      CartProductItem
+  },
+  props: ['cart']
 }
 </script>
 
